26f73e843480f4b0d37b2bdc9380fab461cfa4be
TECNOJACK <jipalaciosc@unac.edu.co>
2021-04-25 18:04:40 -0500

    Se une la app con ultimos cambios

--- Cliente/src/routes.js
+++ Cliente/src/routes.js
@@ -1,4 +1,6 @@
-import { Navigate } from 'react-router-dom';
+import {
+  Navigate
+} from 'react-router-dom';
 import DashboardLayout from 'src/components/DashboardLayout';
 import MainLayout from 'src/components/MainLayout';
 import Account from 'src/pages/Account';
@@ -6,25 +8,50 @@ import CustomerList from 'src/pages/Materias';
 import Dashboard from 'src/pages/Dashboard';
 import Settings from 'src/pages/Settings';
 import Demo from 'src/pages/Calendar';
+import SignIn from 'src/components/login/login'
+import Registroinicio from 'src/components/registro/Inicio'
 
-const routes = [
-  {
-    path: 'app',
-    element: <DashboardLayout />,
-    children: [
-      { path: 'account', element: <Account /> },
-      { path: 'customers', element: <CustomerList /> },
-      { path: 'dashboard', element: <Dashboard /> },
-      { path: 'products', element: <Demo /> },
-      { path: 'settings', element: <Settings /> },
-    ]
+const routes = [{
+    path: 'login',
+    element: < SignIn / >
   },
   {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { path: '/', element: <Navigate to="/app/dashboard" /> }
+    path: 'registro',
+    element: < Registroinicio / >
+  },
+  {
+    path: 'app',
+    element: < DashboardLayout / > ,
+    children: [{
+        path: 'account',
+        element: < Account / >
+      },
+      {
+        path: 'customers',
+        element: < CustomerList / >
+      },
+      {
+        path: 'dashboard',
+        element: < Dashboard / >
+      },
+      {
+        path: 'products',
+        element: < Demo / >
+      },
+      {
+        path: 'settings',
+        element: < Settings / >
+      },
     ]
+  },
+
+  {
+    path: '/',
+    element: < MainLayout / > ,
+    children: [{
+      path: '/',
+      element: < Navigate to = "/app/dashboard" / >
+    }]
   }
 ];
 
