7e5994a64b375ffd8a2da30c6c16de0c7c10e717
danduke99 <72228945+danduke99@users.noreply.github.com>
2021-05-16 13:30:00 -0400

    Add files via upload

---
+++ Cliente/src/components/survey/survey.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import * as Survey from "survey-react";
+// import "../../styles.css";
+// import "survey-react/survey.css";
+
+// export default function SurveyComp() {
+//   const json = {
+//     questions: [
+//       {
+//         type: "checkbox",
+//         name: "desempeño",
+//         title: "¿Como se siente con tu desempeño?",
+//         isRequired: true,
+//         colCount: 5,
+//         choicesOrder: "asc",
+//         choices: [1, 2, 3, 4, 5],
+//       },
+//     ],
+//   };
+//   const surveyRender = <Survey.Survey json={json} />;
+//   return (
+//     <div className="App">
+//       <h1>Autoevaluacion</h1>
+//       {surveyRender}
+//     </div>
+//   );
+// }
