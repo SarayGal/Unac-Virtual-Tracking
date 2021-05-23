590e5676aca5fb3064be29c98a83fd1e864974cf
Nixon Javier Ochoa Perdomo <80726964+Nix0n98@users.noreply.github.com>
2021-05-22 20:07:39 -0500

    Conexión con la API
    Conexión API con la pantalla de registro

--- Cliente/src/registro/Inicio.js
+++ Cliente/src/registro/Inicio.js
@@ -1,43 +1,105 @@
 import './inicio.css';
 import './util.css';
-import './main.php';
 import logo from './images/logo.png';
+import React, {useState } from "react"; 
 
-function registroinicio() {
+
+export default function Registro(){
+    const [state, setState] = useState({
+        form: {
+            name: "", 
+            password: "", 
+            email: "",
+            cellphone: "",
+            type_id: "",
+            documento: "",
+            role: "",
+            genre: "",
+            codigo: "",
+            grade: "",
+        }, 
+    }); 
+
+    const handleChange = (e) => {
+        setState({
+            form:{
+                ...state.form, 
+                [e.target.name]: e.target.value,
+            },
+        }); 
+    }
+
+    const [setShowError] = useState (false); 
+    const requestAuth = async (event) => {
+        event.preventDefault();
+        return fetch ("https://app-unac-virtual-tracking.herokuapp.com/auth/registro" , { 
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: state.form.name,
+                password: state.form.password,
+                cellphone: state.form.cellphone,
+                email: state.form.email,
+                type_id: state.form.type_id,
+                num_id: state.form.num_id,
+                role: state.form.role,
+                genre: state.form.genre,
+                cod_student: state.form.cod_student,
+                grade: state.form.grade,
+            }), 
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.ok) {
+                window.location.assign("https://app-unac-virtual-tracking.herokuapp.com/auth/dashboard");
+            } else {
+                setShowError(true);
+                setTimeout(() => {
+                    setShowError(false);
+                }, 5000);
+            }
+        })
+        .catch((error) => {
+            console.log('ERROR: ', error);
+        }); 
+    }
+
     return (
         <div className="inicio">
             <div class="container-contact100">
                 <div class="wrap-contact100">
-                    <form class="contact100-form validate-form position-absolute" action="main.php" method="post">
+                    <form class="contact100-form validate-form position-absolute">
                         <span class="contact100-form-title">
                         <img src={logo} />
                         </span>
 
                         <div class="wrap-input100 validate-input" data-validate="Tu nombre es requerido">
                             <span class="label-input100" for="name">Tu nombre completo</span>
-                            <input class="input100" type="text" name="name" placeholder="Nombre Completo" id="name" required="true"></input>
+                            <input class="input100" type="text" name="name" placeholder="Nombre Completo" id="name" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
 
                         <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
                             <span class="label-input100" for="email">Correo Electronico</span>
-                            <input class="input100" type="text" name="email" placeholder="Correo Electronico" id="email" required="true"></input>
+                            <input class="input100" type="text" name="email" placeholder="Correo Electrónico" id="email" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
                         <div class="wrap-input100 validate-input" data-validate="Tú número de celular es requerido">
-                            <span class="label-input100" for="celular">Celular</span>
-                            <input class="input100" type="text" name="celular" placeholder="Número de celular" id="celular" required="true"></input>
+                            <span class="label-input100" for="cellphone">Celular</span>
+                            <input class="input100" type="text" name="cellphone" placeholder="Celular " id="cellphone" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
                         <div class="wrap-input100 validate-input" data-validate="La contraseña es obligatoria">
-                            <span class="label-input100" for="contraseña">Escribe tu contraseña</span>
-                            <input class="input100" type="text" name="contraseña" placeholder="Escribe tu contraseña" id="contraseña" required="true"></input>
+                            <span class="label-input100" for="password">Escribe tu contraseña</span>
+                            <input class="input100" type="text" name="password" placeholder="Contraseña" id="password" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
                         <div class="wrap-input100 input100-select">
                             <span class="label-input100">Elige tu documento de identidad</span>
                             <div>
-                                <select class="selection-2" name="documento" id="documento" required="true" data-validate="El tipo de documento es requerido">
+                                <select class="selection-2" name="type_id" id="type_id" required="true" data-validate="El tipo de documento es requerido" onChange={handleChange}>
                                     <option disabled selected>Selecciona una opción</option>
                                     <option value="Tarjeta de Identidad">Tarjeta de Identidad</option>
                                     <option value="Cédula de Ciudadanía">Cédula de Ciudadanía</option>
@@ -51,18 +113,18 @@ function registroinicio() {
                 </div>
                 
                 <div class="wrap-contact100">
-                    <form class="contact100-form validate-form position-absolute" action="main.php" method="post">
+                    <form class="contact100-form validate-form position-absolute">
                         <span class="contact100-form-title">
                         </span>
                         <div class="wrap-input100 validate-input" data-validate="Tú número de documento es requerido">
-                            <span class="label-input100" for="cedula">Documento de Identidad</span>
-                            <input class="input100" type="text" name="cédula" placeholder="Número de documento" id="cédula" required="true"></input>
+                            <span class="label-input100" for="num_id">Documento de Identidad</span>
+                            <input class="input100" type="text" name="num_id" placeholder="Documento" id="num_id" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
                         <div class="wrap-input100 input100-select">
                             <span class="label-input100">Elige tu condición</span>
                             <div>
-                                <select class="selection-2" name="condicion" id="condicion" required="true" data-validate="Este campo es requerido">
+                                <select class="selection-2" name="role" id="role" required="true" onChange={handleChange} data-validate="Este campo es requerido"> 
                                     <option disabled selected>Selecciona una opción</option>
                                     <option value="Docente">Docente</option>
                                     <option value="Estudiante">Estudiante</option>
@@ -73,7 +135,7 @@ function registroinicio() {
                         <div class="wrap-input100 input100-select">
                             <span class="label-input100">Género</span>
                             <div>
-                                <select class="selection-2" name="genero" id="genero">
+                                <select class="selection-2" name="genre" id="genre" onChange={handleChange}>
                                     <option disabled selected>Selecciona tu género</option>
                                     <option value="Femenino">Femenino</option>
                                     <option value="Masculino">Masculino</option>
@@ -84,14 +146,14 @@ function registroinicio() {
                         </div>
                         <h6>Los siguientes campos solo son para estudiantes</h6>
                         <div class="wrap-input100 validate-input" data-validate="Son solo números">
-                            <span class="label-input100" for="código">Código Estudiantil</span>
-                            <input class="input100" type="text" name="códigp" placeholder="Código Estudiantil" id="código" required="true"></input>
+                            <span class="label-input100" for="cod_student">Código Estudiantil</span>
+                            <input class="input100" type="text" name="cod_student" placeholder="Código Estudiantil" id="cod_student" required="true" onChange={handleChange}></input>
                             <span class="focus-input100"></span>
                         </div>
                         <div class="wrap-input100 input100-select">
                             <span class="label-input100">Elige tu grado</span>
                             <div>
-                                <select class="selection-2" name="grado" id="grado" required="true" data-validate="El grado es requerido">
+                                <select class="selection-2" name="grade" id="grade" required="true" data-validate="El grado es requerido" onChange={handleChange}>
                                     <option disabled selected>Selecciona tu grado</option>
                                     <option value="6º">6º</option>
                                     <option value="7º">7º</option>
@@ -106,11 +168,12 @@ function registroinicio() {
                         <div class="container-contact100-form-btn">
                             <div class="wrap-contact100-form-btn">
                                 <div class="contact100-form-bgbtn"></div>
-                                <button class="contact100-form-btn" id="enviar">
-                                    <span>
-                                        Enviar
-								                                <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
-                                    </span>
+                                <button class="contact100-form-btn" 
+                                href="https://app-unac-virtual-tracking.herokuapp.com/auth/dashboard" 
+                                type="submit" 
+                                onClick={requestAuth}>
+                                Enviar
+                                <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
                                 </button>
                             </div>
                         </div>
@@ -132,6 +195,4 @@ function registroinicio() {
             <script src='https://code.jquery.com/jquery-3.2.1.js'></script>
         </div>
     );
-}
-
-export default registroinicio;
+}; 
